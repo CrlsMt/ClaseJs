9caf8c1579d38b65bfe311be929ab025cd0c937b
CrlsMt <l18550717@chihuahua2.tecnm.mx>
2022-10-13 14:20:26 -0600

    1

--- src/index.js
+++ src/index.js
@@ -1,14 +1,3 @@
-import "./styles.css";
-
-document.getElementById("app").innerHTML = `
-<h1>Hello Vanilla!</h1>
-<div>
-  We use the same configuration as Parcel to bundle this sandbox, you can find more
-  info about Parcel 
-  <a href="https://parceljs.org" target="_blank" rel="noopener noreferrer">here</a>.
-</div>
-`;
-
 //Object Literal Syntax
 const circle = {
   //Propiedades
@@ -20,20 +9,19 @@ const circle = {
     y: 2
   },
 
-  //Metodos
+  //Métodos
   draw: function () {
     console.log("Dibujando...");
   }
 };
 
-//Si quisiera crear otro circulo se tendria que duplicar el codigo
-//Cuando un objeto tiene un metodo o más, se dice que tiene comportamiento.
+//Si quisiera crear otro círculo, se tendria que duplicar el código.
+//Cuando un objeto tien un método o más, se dice que tiene comportamiento (behavior)
 
 console.log(circle.location);
 circle.draw();
 
-//Si se requiere crear más de una instancia se puede usar un
-
+//Si se requiere crear más de una instancia, se puede usar un Factory
 function factoryStudent(name, semester) {
   return {
     name,
@@ -49,18 +37,9 @@ const student1 = factoryStudent("Felipe", 4);
 console.log(student1.name);
 student1.greet();
 
-//Tambien se puede con una función constructora
-
-function constructorStudent(name, semester) {
-  this.name_std = name;
-  this.semester = semester;
-  this.subjects = [];
-  this.greet = function(){
-    console.log("Hola", name);
-  };
-}
-
-function constructorStudent(name, semester) {
+//También se puede con una función constructora
+function ConstructorStudent(name, semester) {
+  console.log("This", this);
   this.name_std = name;
   this.semester = semester;
   this.subjects = [];
@@ -68,29 +47,177 @@ function constructorStudent(name, semester) {
     console.log("Hola", name);
   };
 }
-  
-const student2 = constructorStudent("Alex", 2);
+const student2 = new ConstructorStudent("Perengano", 2);
 student2.greet();
-  
-/*Al utilizar el operador new, realiza:
-  1. Crear un objeto vacion con{}
-  2. Asigna this para que apunte a este objeto, porque por defecto apunta al objeto 
-  al objeto global, lo cual es mala practica.
-  3. Devuelve el objeto creado por la funcion el return this se hace implicitamente
- */
-  
 console.log(student1.constructor);
-console.log(student2.constructor);
-  
-/*  Las funciones como los object literal tienen como constructor el de la clase object.
-    Todas las variables se crean a partir de constructores let x = {} viene de 
-    let x = new object()
-    '' viene new String()
-    true viene de new boolean()
-    1 viene de new number().
+
+/*Al utilizar el operador new, realiza:
+1.- Crear un objeto vacío con {}
+2.- Asigna this para que apunte a este objeto, porque por defecto
+apunta al objeto global, lo cual es mala práctica.
+3.- Devuelve el objeto creado por la función
+El return this se hace implícitamente
+ */
+
+const FunctionStudent = new Function(
+  "name, semester",
+  `
+this.name_std = name;
+  this.semester = semester;
+  this.subjects = [];
+  this.greet = function () {
+    console.log("Hola", name);
+  };
+`
+);
+const functionStudent = new FunctionStudent("Zutano Algo", 5);
+ConstructorStudent.call({}, "Fulanito", 3);
+ConstructorStudent.apply({}, ["Manganito", 7]);
+
+/*
+Los elementos en Javascript se clasifican en 2 tipos:
+ - Value Types.- Number, String, Boolean, Symbol, undefined, null
+ - Reference Types.- Object, Function, Array
+ Como los Function y Array también son objetos, los Reference Types son solamente objetos.
 */
 
-/*const functionStudent = new function('name', 'semester', '
+let x = 10;
+let y = x;
+y = 20;
+console.log(x); //x Y y son totalmente independientes porque se copian por valor.
 
-')*/
+let xo = { value: 10 };
+let yo = xo;
+yo.value = 20;
+console.log(xo); // xo y yo están enlazados porque están uniendo su referencia.
 
+let n = 15;
+function duplicate(number) {
+  number *= 2;
+}
+duplicate(n);
+console.log("Duplicate con Number:", n);
+
+let no = { value: 15 };
+function duplicateObj(obj) {
+  obj.value *= 2;
+}
+duplicateObj(no);
+console.log("Duplicate con Object:", no);
+
+/*AÑADIR COSAS A UN OBJETO */
+student2.control = "09550222";
+student2["nip"] = "1324";
+
+student2["otra-cosa"] = "asndoi";
+//student2.algo-mas = "asjd nsdiufn"; NO SE PUEDE
+
+//ITERAR ELEMENTOS DE UN OBJETO
+console.log("Iterar elementos de student2:-------");
+let key;
+for (key in student2) {
+  //console.log(key, student2[key])
+}
+
+console.log("Solamente las propiedades:-----");
+
+for (key in student2) {
+  if (typeof student2[key] !== "function") {
+    //console.log(key, student2[key])
+  }
+}
+
+const keys = Object.keys(student2);
+//console.log("Desde Object.keys", keys);
+
+//También se puede con una función constructora
+function StudentProperties(name, semester) {
+  //console.log("This", this);
+  this.name_std = name;
+  let pin = "1234"; //Propiedad privada
+  this.semester = semester;
+  this.subjects = [];
+  this.hash = function () {
+    let s = pin.split("");
+    let num = s.map(Number);
+    const helperSum = function (acc, curr) {
+      return acc + curr;
+    };
+    return num.reduce(helperSum, 0);
+  };
+
+  this.toString = function () {
+    //Shadowing
+    return this.name_std;
+  };
+
+  this.greet = function () {
+    console.log("Hola", name);
+  };
+
+  this.getPin = function () {
+    return "El pin del alumno es: " + pin;
+  };
+
+  this.setPin = function (p) {
+    const num = Number(p);
+
+    if (Number.isInteger(num) && num > 0) {
+      pin = num.toString();
+      return;
+    }
+
+    throw new Error("Dato inválido.");
+  };
+
+  Object.defineProperty(this, "pin", {
+    get: function () {
+      return "El pin del alumno es: " + pin;
+    },
+
+    set: function (p) {
+      const num = Number(p);
+
+      if (Number.isInteger(num) && num > 0) {
+        pin = num.toString();
+        return;
+      }
+
+      throw new Error("Dato inválido.");
+    }
+  });
+}
+
+const studentProperties = new StudentProperties("Alberto", 2);
+
+console.log("pin", studentProperties.hash());
+
+//PROTOTYPE CHAIN
+let object = window;
+do {
+  object = Object.getPrototypeOf(object);
+  console.log(object);
+} while (object);
+
+//Shadowing
+studentProperties.toString(); //Alberto
+//Permite sobreescribir propiedades o métodos que estaban
+//en el Prototype para que se adapten a las necesidades del
+//desarrollador.
+
+//Asingar un prototype
+const studentProto = {
+  evaluation: function () {
+    console.log("El estudiante está evaluando...");
+  }
+};
+
+function StudentConstructor(name_std) {
+  this.name_std = name_std;
+  this.carrera = "ISC";
+}
+
+StudentConstructor.prototype = studentProto;
+StudentConstructor.prototype.constructor = StudentConstructor;
+
+const studentOtro = new StudentConstructor("Pepito");
